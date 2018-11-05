e32d796b74173d29205590fd5798d11e2fca387e
M <603803799@qq.com>
2018-11-05 09:41:55 +0800

    Update index.js

--- server/router/index.js
+++ server/router/index.js
@@ -2,6 +2,7 @@
 const Router = require('koa-router')
 const validator = require('../middleware/validator')
 const {resolve, getFiles} = require('../../server/util/common')
+
 const files = getFiles(resolve('server','router')).filter(item => !~item.indexOf('index.js'))
 const routes = files.map(path => require(path))
 
