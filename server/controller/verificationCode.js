ed4048120b453504292a2beeb1a461c00de63cf9
M <603803799@qq.com>
2018-11-16 20:46:42 +0800

    Create verificationCode.js

---
+++ server/controller/verificationCode.js
@@ -0,0 +1,38 @@
+const codeObjects = {}
+
+const options = {
+  delay: 120, // 验证码有效时间[Minute] /* 1 < delay < 35791.39411 */
+}
+
+exports.config = option => Object.assign(options, option)
+
+function randomString (len = 6) { // 随机字符串6
+  const $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678'
+  const maxPos = $chars.length
+  /** **默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+  let pwd = ''
+  for (let i = 0; i < len; i++) pwd += $chars.charAt(Math.floor(Math.random() * maxPos))
+  return pwd
+}
+
+exports.set = (key, len) => {
+  const value = randomString(len)
+  codeObjects[key || value] = value
+  const timer = setTimeout(() => {
+    delete codeObjects[key]
+    clearTimeout(timer)
+  }, options.delay * 60000)
+}
+
+exports.get = code => {
+  let keyAssignments = ''
+  const record = Object.entries(codeObjects).find(([key, value]) => {
+    if (code === value) {
+      keyAssignments = key
+      return true
+    }
+    return false
+  })
+  if (record) delete codeObjects[keyAssignments]
+  return record
+}
